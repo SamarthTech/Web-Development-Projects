cdbb0c0740008fb826af549aa682746349ec21fa
Ejay02 <e.jae02@gmail.com>
2023-10-08 10:43:31 +0100

    Random Password Generator

---
+++ Password Generator/index.js
@@ -0,0 +1,27 @@
+const passwordBox = document.getElementById("password");
+const length = 12; //password length
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@!~#$%^&*()_-+=}{][|><?/";
+
+const allChars = upperCase + lowerCase + symbol + number;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+
+function copy() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
